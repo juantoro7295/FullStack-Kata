6fb5821d1e61209599316d6c63b0ed6483bbabf4
juantoro7295 <juanpatoro2011@gmail.com>
2022-05-04 09:30:39 -0500

    Crud Spring boot
    se sube crud fucional del todo, spring boot crud, configuracion base de datos Mysql , nombre base: db_kata

--- backend/src/main/java/co/com/sofka/backend/kata/controller/TodoController.java
+++ backend/src/main/java/co/com/sofka/backend/kata/controller/TodoController.java
@@ -1,4 +1,46 @@
 package co.com.sofka.backend.kata.controller;
 
+import co.com.sofka.backend.kata.model.Todo;
+import co.com.sofka.backend.kata.service.impl.TodoServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:3000")
+@RequestMapping(value = "/api/todo")
 public class TodoController {
+
+    @Autowired
+    private TodoServiceImpl todoServiceImpl;
+
+    @GetMapping("/list")
+    public Iterable<Todo> list(){
+        return todoServiceImpl.list();
+    }
+    @PostMapping("/save")
+    public Todo save( @RequestBody Todo todo){
+        return todoServiceImpl.save(todo);
+    }
+    @PutMapping("/update")
+    public Todo update (@RequestBody Todo todo){
+        if(todo.getId() != null){
+            todoServiceImpl.save(todo);
+        }
+        throw new RuntimeException("No existe el todo");
+    }
+    @GetMapping("/get/{id}")
+    public Optional<Todo> getId(@PathVariable("id") Long id){
+        return todoServiceImpl.getId(id);
+    }
+    @DeleteMapping("/delete/{id}")
+    public void delete(@PathVariable("id") Long id){
+        todoServiceImpl.delete(id);
+    }
+
+
+
+
+
 }
