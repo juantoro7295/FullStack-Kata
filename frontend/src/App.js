b4da536d0291538cbb3f83dc6bcbc705ea89c941
juantoro7295 <juanpatoro2011@gmail.com>
2022-05-04 13:08:32 -0500

    Estructura en App.js
     se creo el const list y el const form y se creo la funtion reducer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,130 @@
 import './App.css';
-import React from 'react';
+import React, { useContext, createContext, useEffect, useReducer, useRef, useState } from 'react';
+
+const HOST_API = "http://localhost:8080/api/todo";
+const initialState = {
+  list: [],
+};
+
+const Store = createContext(initialState);
+
+const Form = () => {
+  const formRef = useRef(null);
+  const { dispatch } = useContext(Store);
+  const [state, setState] = useState({});
+
+  const onAdd = (event) => {
+    event.preventDefault();
+
+    const request = {
+      name: state.name,
+      id: null,
+      isCOmpleted: false,
+    };
+
+    fetch(HOST_API + "/save", {
+      method: "POST",
+      body: JSON.stringify(request),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((todo) => {
+        dispatch({ type: "add-item", item: todo });
+        setState({ name: "" });
+        formRef.current.reset();
+      });
+  };
+  return (
+    <form ref={formRef}>
+      <input
+        type="text"
+        name="name"
+        onChange={(event) => {
+          setState({ ...state, name: event.target.value });
+        }}
+      ></input>
+      <button onClick={onAdd}>Agregar</button>
+    </form>
+  );
+};
+
+
+
+const List = () => {
+  const { dispatch, state } = useContext(Store);
+
+  useEffect(() => {
+    fetch(HOST_API + "/list")
+      .then((response) => response.json())
+      .then((list) => {
+        dispatch({ type: "update-list", list });
+      });
+  }, [state.list.length, dispatch]);
+  return (
+    <table>
+      <thead>
+        <tr>
+          <td>ID</td>
+          <td>Nombre</td>
+          <td>Esta completado?</td>
+        </tr>
+      </thead>
+      <tbody>
+        {state.list.map((todo) => {
+          return (
+            <tr key={todo.id}>
+              <td>{todo.id}</td>
+              <td>{todo.name}</td>
+              <td>{todo.isCompleted}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "delete-item":
+      const listUpdate = state.filter((item) => {
+        return item.id !== action.id;
+      });
+      return { ...state, list: listUpdate };
+    case "update-list":
+      return { ...state, list: action.list };
+    case "edit-item":
+      return { ...state, item: action.item };
+    case "add-item":
+      const newList = state.list;
+      newList.push(action.item);
+      return { ...state, list: newList };
+    default:
+      return state;
+  }
+}
+
+const StoreProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <Store.Provider value={{ state, dispatch }}>{children}</Store.Provider>
+  );
+};
+
 
 
 function App() {
+  
   return (
-    <div className="App">
-     
-    </div>
+    <StoreProvider>
+      <Form />
+      <List />
+    </StoreProvider>
   );
-}
-
+};
 export default App;
+
+
